60a6b5cd138b4f28e5b8c47a30e94e2edc1e56c4
rifatbit0401 <rifat92iitdu@gmail.com>
2018-01-14 17:49:17 +0600

    publishing files in a folder for public access

--- TestConsoleCoreApp/TestConsoleCoreApp/ItemController.cs
+++ TestConsoleCoreApp/TestConsoleCoreApp/ItemController.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc; 
 
 namespace TestConsoleCoreApp
 {
--- TestConsoleCoreApp/TestConsoleCoreApp/Startup.cs
+++ TestConsoleCoreApp/TestConsoleCoreApp/Startup.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
 
 namespace TestConsoleCoreApp
 {
@@ -24,6 +27,15 @@ namespace TestConsoleCoreApp
 
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            app.UseStaticFiles();
+            app.UseStaticFiles(new StaticFileOptions()
+            {
+                FileProvider = new PhysicalFileProvider(
+                    Path.Combine(Directory.GetCurrentDirectory(), @"upload")),
+                RequestPath = new PathString("/upload")
+            });
+
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
